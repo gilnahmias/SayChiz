e7629ec7d9d97a9b6199ffb87311ee5fb096bf71
Gil Nahmias <gilna@Gils-MacBook-Pro.local>
2016-06-29 22:48:57 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function(){
+  $("footer").on('click', function(){
+    $("#upload").trigger('click');
+  });
+
+  $("#upload").on("change", function() {
+    readURL(this);
+  });
+});
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#pic').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
