7998a17cf1ecb78de113ff4dae13ff17de0e9d95
Gil Nahmias <gilna@Gils-MacBook-Pro.local>
2016-07-03 10:13:48 -0700

    working

--- script.js
+++ script.js
@@ -3,50 +3,54 @@ function openFileDialog (){
 }
 
 function readURL(files) {
-    if (files[0]) {
-        var reader = new FileReader();
-        reader.onload = onImageLoad;
-        reader.readAsDataURL(files[0]);
-    }
+  if (files[0]) {
+    var emotionReader = new FileReader();
+    emotionReader.onload = getEmotion;
+    emotionReader.readAsArrayBuffer(files[0]);
+
+    var previewReader = new FileReader();
+    previewReader.onload = preview;
+    previewReader.readAsDataURL(files[0]);
+  }
 }
 
-function onImageLoad (e) {
-    $('#pic').attr('src', e.target.result);
+function preview(e){
+  $('#pic').attr('src', e.target.result);
+}
 
-    // var data = new FormData();
-    // $.each(files, function(key, value)
-    // {
-    //     data.append(key, value);
-    // });
+function getEmotion (e) {
+  $(".title").text("Let me see...");
 
-    $.ajax({
-      url : 'https://api.projectoxford.ai/emotion/v1.0/recognize',
-      type: 'POST',
-      dataType : "json",
-      data: {}, // {"images": e.target.result},
-      headers: {
-        "Ocp-Apim-Subscription-Key": "e0b5b29480894ae6b192b98380dd550e",
-        //"Content-Type": "application/octet-stream"
-      },
-      success: function(data, textStatus, jqXHR)
-          {debugger;
-              if(typeof data.error === 'undefined')
-              {
-                  // Success so call function to process the form
-                  submitForm(event, data);
-              }
-              else
-              {
-                  // Handle errors here
-                  console.log('ERRORS: ' + data.error);
-              }
-          },
-          error: function(jqXHR, textStatus, errorThrown)
-          {
-            debugger;
-              // Handle errors here
-              console.log('ERRORS: ' + textStatus);
-              // STOP LOADING SPINNER
-          }
-    });
+  $.ajax({
+    url : 'https://api.projectoxford.ai/emotion/v1.0/recognize',
+    type: 'POST',
+    processData: false,
+    contentType: "application/octet-stream",
+    data: e.target.result,
+    headers: {
+      "Content-Type": "application/octet-stream",
+      "Ocp-Apim-Subscription-Key": "e0b5b29480894ae6b192b98380dd550e"
+    },
+    success: displayEmotion,
+    error: function(jqXHR, textStatus, errorThrown)
+    {
+      debugger;
+    }
+  });
+}
+
+function displayEmotion(data){
+  var scores = data[0].scores || {};
+
+  var mostLikelyEmotion = "";
+  var maxScore = 0;
+
+  Object.keys(scores).forEach(function(emotion){
+    if (scores[emotion] > maxScore){
+      maxScore = scores[emotion];
+      mostLikelyEmotion = emotion;
+    }
+  });
+
+  $(".title").text("I sense " + mostLikelyEmotion);
 }
